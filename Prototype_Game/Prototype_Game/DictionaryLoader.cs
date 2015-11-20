a595279a926d6cb0446d3359f0075d9493df713c
Venkatpandey <venkatrpandey@yahoo.com>
2015-11-20 16:47:43 +0100

    new project added
    dictonary function initialized

---
+++ Prototype_Game/Prototype_Game/DictionaryLoader.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Prototype_Game
+{
+    public class DictionaryLoader
+    {
+        public class entries
+        {
+            [XmlAttribute("letter")]
+            public string letter { get; set; }
+
+            [XmlElement("entry")]
+            public List<entry> keyEntry = new List<entry>();
+        }
+
+        public class entry
+        {
+            [XmlAttribute("key")]
+            public string keyWords { get; set; }
+
+            //[XmlElement("def")]
+            //public List<string> keyDefinition = new List<string>();
+        }
+
+
+        public List<string> xEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_x-entries.xml");
+           var result = (entries)deserializer.Deserialize(reader);
+
+           var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+           return dict;
+        }
+
+        public List<string> aEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_a-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> bEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_b-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> cEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_c-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> dEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_d-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> eEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_e-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> fEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_f-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> gEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_g-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> hEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_h-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> iEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_i-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> jEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_j-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> kEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_k-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> lEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_l-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> mEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_m-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> nEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_n-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> oEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_o-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> pEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_p-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> qEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_q-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> rEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_r-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> sEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_s-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> tEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_t-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> uEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_u-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> vEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_v-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> wEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_w-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> yEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_y-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+        public List<string> zEntries()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(entries));
+            TextReader reader = new StreamReader(@"gcide_z-entries.xml");
+            var result = (entries)deserializer.Deserialize(reader);
+
+            var dict = result.keyEntry.Select(xx => xx.keyWords).ToList();
+            return dict;
+        }
+
+    }
+}
---
+++ Prototype_Game/Prototype_Game/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prototype_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DictionaryLoader dictLoader = new DictionaryLoader();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnReply_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> xletters = dictLoader.xEntries();
+            List<string> aletters = dictLoader.aEntries();
+            List<string> bletters = dictLoader.bEntries();
+            List<string> cletters = dictLoader.cEntries();
+            List<string> dletters = dictLoader.dEntries();
+            List<string> eletters = dictLoader.eEntries();
+            List<string> fletters = dictLoader.fEntries();
+            List<string> gletters = dictLoader.gEntries();
+            List<string> hletters = dictLoader.hEntries();
+            List<string> iletters = dictLoader.iEntries();
+            List<string> jletters = dictLoader.jEntries();
+            List<string> kletters = dictLoader.kEntries();
+            List<string> lletters = dictLoader.lEntries();
+            List<string> mletters = dictLoader.mEntries();
+            List<string> nletters = dictLoader.nEntries();
+            List<string> oletters = dictLoader.oEntries();
+            List<string> pletters = dictLoader.pEntries();
+            List<string> qletters = dictLoader.qEntries();
+            List<string> rletters = dictLoader.rEntries();
+            List<string> sletters = dictLoader.sEntries();
+            List<string> tletters = dictLoader.tEntries();
+            List<string> uletters = dictLoader.uEntries();
+            List<string> vletters = dictLoader.vEntries();
+            List<string> wletters = dictLoader.wEntries();
+            List<string> yletters = dictLoader.yEntries();
+            List<string> zletters = dictLoader.zEntries();
+            
+
+
+            string match = "Xenomi";
+
+            var x = xletters.Find(mm => mm == match);
+
+            txtReturn.Text += x;
+        }
+    }
+}
