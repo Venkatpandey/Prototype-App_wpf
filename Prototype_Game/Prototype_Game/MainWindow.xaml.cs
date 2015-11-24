9eda0d0b68a1d0f5f61d5c16c157f8f7d5b84a2f
Venkatpandey <venkatrpandey@yahoo.com>
2015-11-24 15:15:09 +0100

    gamerule class added
    game rule class added to  define game rules and boundaries

--- Prototype_Game/Prototype_Game/MainWindow.xaml.cs
+++ Prototype_Game/Prototype_Game/MainWindow.xaml.cs
@@ -20,49 +20,49 @@ namespace Prototype_Game
     /// </summary>
     public partial class MainWindow : Window
     {
-        DictionaryLoader dictLoader = new DictionaryLoader();
+        Game game = new Game();
+        GameRules gamerule = new GameRules();
         public MainWindow()
         {
             InitializeComponent();
-
+            
         }
 
         private void btnReply_Click(object sender, RoutedEventArgs e)
         {
-            List<string> xletters = dictLoader.xEntries();
-            List<string> aletters = dictLoader.aEntries();
-            List<string> bletters = dictLoader.bEntries();
-            List<string> cletters = dictLoader.cEntries();
-            List<string> dletters = dictLoader.dEntries();
-            List<string> eletters = dictLoader.eEntries();
-            List<string> fletters = dictLoader.fEntries();
-            List<string> gletters = dictLoader.gEntries();
-            List<string> hletters = dictLoader.hEntries();
-            List<string> iletters = dictLoader.iEntries();
-            List<string> jletters = dictLoader.jEntries();
-            List<string> kletters = dictLoader.kEntries();
-            List<string> lletters = dictLoader.lEntries();
-            List<string> mletters = dictLoader.mEntries();
-            List<string> nletters = dictLoader.nEntries();
-            List<string> oletters = dictLoader.oEntries();
-            List<string> pletters = dictLoader.pEntries();
-            List<string> qletters = dictLoader.qEntries();
-            List<string> rletters = dictLoader.rEntries();
-            List<string> sletters = dictLoader.sEntries();
-            List<string> tletters = dictLoader.tEntries();
-            List<string> uletters = dictLoader.uEntries();
-            List<string> vletters = dictLoader.vEntries();
-            List<string> wletters = dictLoader.wEntries();
-            List<string> yletters = dictLoader.yEntries();
-            List<string> zletters = dictLoader.zEntries();
+           
+            string userWord = txtReturn.Text;
+            string retWord = gamerule.wordPlayedByUser(userWord.ToLower());
+            txtHistory.Text += "User Word: " + retWord + Environment.NewLine;
+            //game.playedWord(userWord.ToLower());
+
+            string systemWord = game.getReply(retWord);
+            string retSysWord = gamerule.playedWord(systemWord.ToLower());
+            txtHistory.Text += "            System Word: " + retSysWord + Environment.NewLine;
             
+            //game.playedWord(systemWord.ToLower());
+            //var x = xletters.Find(mm => mm == match);
 
-
-            string match = "Xenomi";
-
-            var x = xletters.Find(mm => mm == match);
-
-            txtReturn.Text += x;
+            txtReturn.Text = string.Empty;
         }
+
+        private void txtReturn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void newGame_Click(object sender, RoutedEventArgs e)
+        {
+            string newWord = game.gameInitiator();
+            string retSysWord = gamerule.playedWord(newWord);
+            txtHistory.Text += "            System Word: " + retSysWord + Environment.NewLine;
+            
+        }
+
     }
 }
